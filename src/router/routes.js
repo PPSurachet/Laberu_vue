434eaa37857498486da51b725e8564cb5589a90e
armnutthapon <armnutthapon@hotmail.co.th>
2021-03-16 23:15:06 +0700

    add profilePage

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,28 +1,32 @@
 const routes = [{
-        path: '/',
-        component: () =>
-            import ('layouts/MainLayout.vue')
-    },
-    {
-        path: '/index',
-        component: () =>
-            import ('pages/Index.vue')
-    },
-    {
-        path: '/history',
-        component: () =>
-            import ('pages/History.vue')
-    },
-    {
-        path: '/register',
-        component: () =>
-            import ('pages/Register.vue')
-    },
-    {
-        path: '*',
-        component: () =>
-            import ('pages/Error404.vue')
-    }
+    path: '/',
+    component: () =>
+        import('layouts/MainLayout.vue')
+},
+{
+    path: '/index',
+    component: () =>
+        import('pages/Index.vue')
+},
+{
+    path: '/history',
+    component: () =>
+        import('pages/History.vue')
+},
+{
+    path: '/register',
+    component: () =>
+        import('pages/Register.vue')
+}, {
+    path: '/profile',
+    component: () =>
+        import('pages/Profile.vue')
+},
+{
+    path: '*',
+    component: () =>
+        import('pages/Error404.vue')
+},
 ]
 
 export default routes
