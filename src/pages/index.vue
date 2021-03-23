546117e2f193d6f78cc5d9becadd89f7621f64a1
Lisardgy <62231142+Lisardgy@users.noreply.github.com>
2021-03-24 01:42:27 +0700

    fix scrollable edit css scope profBtn add backBtn

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -27,7 +27,7 @@
               <q-list style="min-width: 100px">
                 <q-item v-close-popup>
                   <q-btn
-                    color="amber"
+                    class="profileBtn"
                     label="Profile"
                     @click="$router.push('/profile')"
                     size="md"
@@ -49,7 +49,7 @@
         </q-toolbar>
       </div>
     </q-header>
-    <backgroundDisplay> </backgroundDisplay>
+    <backgroundDisplay></backgroundDisplay>
 
     <q-page-container style="padding-top: 0">
       <div class="context">
@@ -127,9 +127,7 @@
                   style="max-width: 90% align-item-center"
                   row="100"
                 >
-                <q-form
-                  @submit="onSubmit"
-                  >
+                
                   <q-input
                     filled
                     class="textDescribe"
@@ -446,195 +444,9 @@ export default {
     }
   },
 };
-import backgroundDisplay from '../components/login_animation'
-export default{
-  components:{
-    backgroundDisplay
-  },
-  data(){
-    return{
-    deciptionArea: null,
-    }
-  },
-  methods: {
-    onSubmit () {
-      if (this.accept !== true) {
-        this.$q.notify({
-          color: 'red-5',
-          textColor: 'white',
-          icon: 'warning',
-          message: 'You need to accept the license and terms first'
-        })
-      }
-      else {
-        this.$q.notify({
-          color: 'green-4',
-          textColor: 'white',
-          icon: 'cloud_done',
-          message: 'Submitted'
-        })
-      }
-    },
-  }
-}
 </script>
 
-<style>
-.exampleWay input {
-  font-size: 15px;
-  font-weight: 500;
-  opacity: 1;
-}
-.exampleWay {
-  height: 5rem;
-}
-.navUsername {
-  color: black;
-}
-.imgNumber {
-  padding: 5px 0 0 15px;
-  font-weight: bold;
-  font-size: 18px;
-}
-.imgID {
-  padding: 0 15px 0 0;
-  text-align: right;
-  font-size: 10px;
-}
-.detail {
-  padding: 5px 0 0 20px;
-  font-weight: bold;
-  font-size: 18px;
-}
-.ProfileIMG {
-  color: black;
-  padding: 0 0 0 50px;
-  font-size: 40px;
-}
-.iconIMG {
-  font-size: 50px;
-  padding: 0 0 0 50px;
-}
-.cardIMG {
-  width: 700px;
-  border-radius: 10px;
-}
-.cardText {
-  width: 500px;
-  border-radius: 10px;
-}
-.cardProfile {
-  margin: 30px 0 0 0;
-  width: 500px;
-  height: 200px;
-  border-radius: 10px;
-}
-.imgMain {
-  width: 100%;
-  border-radius: 5px;
-}
-.imgID {
-  margin: -10px 0 0 0;
-  font-size: 10px;
-}
-.q-pa-md {
-  padding: 16px 16px;
-  margin: -20px 0 0 0;
-}
-.btnSkip {
-  border-radius: 5px;
-  font-size: 12px;
-  width: 20%;
-  height: 90%;
-}
-.btnColor {
-  width: 90%;
-  background-color: #6bce2e;
-  color: white;
-}
-.textDescribe {
-  resize: none !important;
-}
-.q-textarea .q-field__native {
-  resize: none;
-  padding-top: 17px;
-  min-height: 52px;
-}
-.btnSave {
-  padding: 10px 10px 10px 10px;
-  text-align: center;
-}
-.toolbarT {
-  height: 80px;
-  background: #f8f8f8;
-}
-.titleName {
-  color: #666877;
-}
-.my-card {
-  width: 400px;
-  border-radius: 10px;
-}
-.context {
-  width: 100%;
-  position: absolute;
-  top: 10rem;
-}
-.iconic {
-  /* Group 65 */
-  position: absolute;
-  width: 30px;
-  height: 30px;
-  box-shadow: inset 0px 0px 62px rgba(0, 0, 0, 0.25);
-  /* Ellipse 74 */
-  position: absolute;
-  width: 30px;
-  height: 30px;
-  background: #6d6b8a;
-  /* Group 64 */
-  position: absolute;
-  width: 16.62px;
-  height: 19.56px;
-  /* Rectangle 128 */
-  position: absolute;
-  width: 1.8px;
-  height: 15.21px;
-  background: #d15eff;
-  border-radius: 3px;
-  /* Rectangle 130 */
-  position: absolute;
-  width: 1.8px;
-  height: 10.37px;
-  background: #d15eff;
-  border-radius: 3px;
-  /* Rectangle 132 */
-  position: absolute;
-  width: 1.8px;
-  height: 6.57px;
-  background: #d15eff;
-  border-radius: 3px;
-  /* Rectangle 129 */
-  position: absolute;
-  width: 1.8px;
-  height: 5.65px;
-  background: #d15eff;
-  border-radius: 3px;
-  transform: rotate(-90deg);
-  /* Rectangle 131 */
-  position: absolute;
-  width: 2.18px;
-  height: 16.62px;
-  background: #d15eff;
-  border-radius: 3px;
-  transform: rotate(-90deg);
-  /* Rectangle 133 */
-  position: absolute;
-  width: 1.8px;
-  height: 16.62px;
-  background: #d15eff;
-  border-radius: 3px;
-  transform: rotate(-90deg);
-}
+<style scoped src="../css/index.css">
 </style>
 
 
